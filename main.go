99cf8c1fdb3ef7fd2fb0aeef2d2f8ce0c7fd27c6
Bill Coll <59691769+bcschwimm@users.noreply.github.com>
2021-05-29 08:48:52 -0400

    formData handler added and working

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 // HunterAnalytics API will collect/serve 5 data points. Time Spent Playing, Training, Exercising, Woofing, Date.
+// use regex must compile to validate form
 
 package main
 
@@ -7,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -15,6 +17,7 @@ func main() {
 	http.HandleFunc("/", index)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.HandleFunc("/api", HunterAPI)
+	http.HandleFunc("/form", formData)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -22,6 +25,25 @@ func main() {
 // index serves our index.html file to the homepage
 func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "assets/index.html")
+}
+
+// formData is recieving a post request from html, parsing
+// that data and redirecting the user to the home page
+func formData(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if r.Method == "POST" {
+		formData := Metrics{
+			Playing:    intConv(r.FormValue("playing")),
+			Training:   intConv(r.FormValue("training")),
+			Exercising: intConv(r.FormValue("exercising")),
+			Woofing:    intConv(r.FormValue("woofing")),
+			Date:       r.FormValue("date"),
+		}
+		// insert into db here
+		fmt.Println(formData)
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
 }
 
 // HunterAPI is serving our cloudsql data from the hunter table
@@ -47,4 +69,12 @@ func HunterAPI(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write(data); err != nil {
 		log.Println("error writing content to response", err)
 	}
+}
+
+func intConv(formSubmission string) int {
+	i, err := strconv.Atoi(formSubmission)
+	if err != nil {
+		fmt.Println("error converting string from html form", err)
+	}
+	return i
 }
