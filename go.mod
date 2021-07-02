41f14d347f0636046360db2336dcc110d3ce0d8e
Bill Coll <59691769+bcschwimm@users.noreply.github.com>
2021-07-01 21:20:09 -0400

    mongo support

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/jackc/pgx/v4 v4.11.0
 	github.com/lib/pq v1.10.2 // indirect
+	go.mongodb.org/mongo-driver v1.5.3
 )
---
+++ mongo.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Behavior struct {
+	ID    string `json:"id,omitempty" bson:"id"`
+	Date  string `json:"date" bson:"date"`
+	Crate int    `json:"crate" bson:"crate"`
+	Notes string `json:"notes" bson:"notes"`
+}
+
+// behavior insert adds a record to our metrics collection in mongoDb
+func (b Behavior) insert() {
+	pass := "uri_string_placeholder" // mongoPass()
+	client, err := mongo.NewClient(options.Client().ApplyURI(pass))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	hunterDatabase := client.Database("hunter")
+	hunterCollection := hunterDatabase.Collection("metrics")
+	hunterInsert, err := hunterCollection.InsertOne(ctx, bson.D{
+		{Key: "date", Value: b.Date},
+		{Key: "crate", Value: b.Crate},
+		{Key: "notes", Value: b.Notes},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(hunterInsert)
+}
