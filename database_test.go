74905c13f8cd5aab60bd1a09a0134a05b1b73877
Bill Coll <59691769+bcschwimm@users.noreply.github.com>
2021-07-03 09:01:47 -0400

    sql rows mocking testing support

--- database_test.go
+++ database_test.go
@@ -1,6 +1,39 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestList(t *testing.T) {
+	want := []Metrics{{7, 7, 7, 7, "06/29/2021"}, {10, 10, 10, 10, "05/29/2021"}}
+	got := []Metrics{}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error %s was not expected opening stub db connection", err)
+	}
+	defer db.Close()
+
+	// mocking our sql rows to test that list is parsing returned rows correctly into []Metrics{}
+	rows := sqlmock.NewRows([]string{"playing", "training", "exercising", "woofing", "date"}).
+		AddRow(7, 7, 7, 7, "06/29/2021").
+		AddRow(10, 10, 10, 10, "05/29/2021")
+
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	rs, _ := db.Query("SELECT")
+
+	for rs.Next() {
+		var testMetric Metrics
+		rs.Scan(&testMetric.Playing, &testMetric.Training, &testMetric.Exercising, &testMetric.Woofing, &testMetric.Date)
+		got = append(got, testMetric)
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Unexpected Metrics Struct output for List got %v want %v", got, want)
+	}
+}
 
 func TestIntConv(t *testing.T) {
 	var tests = []struct {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bcschwimm/hunteranalytics
 go 1.16
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/jackc/pgx/v4 v4.11.0
 	github.com/lib/pq v1.10.2 // indirect
 	go.mongodb.org/mongo-driver v1.5.3
